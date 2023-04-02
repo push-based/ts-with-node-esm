82ed3c6ff7b20c94197270d19bd1f2116aa0e1e8
Michael Hladky <michael@hladky.at>
2023-04-02 22:12:34 +0200

    wip

--- packages/sample-cts/lib/cli.js
+++ packages/sample-cts/lib/cli.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const foo_1 = require("./foo");
--- packages/sample-cts/src/cli.ts
+++ packages/sample-cts/src/cli.ts
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+
 import {getValue} from "./foo";
 
 
