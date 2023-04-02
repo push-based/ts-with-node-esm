a801610907ade2d5e3fc07c500d2525f2435e0f0
Michael Hladky <michael@hladky.at>
2023-04-02 21:07:26 +0200

    wip

---
+++ packages/sample-mts/src/foo.ts
@@ -0,0 +1,21 @@
+import {startFlow} from 'lighthouse';
+import {Browser, launch, Page} from 'puppeteer';
+import getYargs from 'yargs';
+const yargs = getYargs();
+
+export const value = 2;
+
+export const getValue = () => value;
+
+export default function foo() {
+  return 'foo';
+}
+
+export async function startFlowCustom() {
+  console.log('yargs', yargs.argv);
+  const browser: Browser = await launch({headless: false});
+  const page: Page = await browser.newPage();
+  const flow = await startFlow(page);
+  flow.navigate("https://google.com")
+
+}
---
+++ packages/sample-mts/src/index.ts
@@ -0,0 +1,13 @@
+import foo, {value, getValue, startFlowCustom} from './foo.js';
+
+export const run = () => {
+  return value + getValue();
+}
+
+export const runFoo = () => foo();
+
+console.log('run', run());
+
+startFlowCustom().then(() => {
+  console.log('runFoo', runFoo());
+});
